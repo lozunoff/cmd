f2c550c7ee9e7dc49b967972fd12e7f888a1b8b5
lozunoff <andrey.lozunov@gmail.com>
2022-11-24 21:52:21 +0300

    Добавлен базовый класс для команд

---
+++ cmd/CommandBase.cs
@@ -0,0 +1,40 @@
+﻿namespace cmd
+{
+    public abstract class CommandBase
+    {
+        protected abstract string[] allowedArgs { get; }
+
+        protected bool CheckArgs(string[] args)
+        {
+            bool isValidArgs = true;
+
+            foreach (string arg in args)
+            {
+                if (!allowedArgs.Contains(arg))
+                {
+                    Console.WriteLine("Неизвестные аргументы");
+                    isValidArgs = false;
+                    break;
+                }
+            }
+
+            return isValidArgs;
+        }
+
+        protected void PrintResult(List<string> result, string stdout)
+        {
+            if (stdout != "")
+            {
+                Stdout.Write(stdout, result);
+            }
+            else
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, result));
+            }
+        }
+
+        protected abstract void PrintHelp(string stdout);
+
+        public abstract void Execute(string[] targets, string[] args, string[] stdout);
+	}
+}
